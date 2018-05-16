c57d5c5d73ac60a3bb8dcf9d1f9dab6e64cac294
qbstance <yaroslavsalata@gmail.com>
2018-05-16 17:07:58 +0300

    clion proj init

---
+++ cmake-build-debug/CMakeFiles/kpi_open.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kpi_open.dir/main.c.o"
+  "kpi_open.pdb"
+  "kpi_open"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/kpi_open.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
